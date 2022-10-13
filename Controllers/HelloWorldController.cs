1eb3aff8f9a67ab3bc22bf055f0d79178ffb631e
W0778098 <W0778098@myscc.ca>
2022-10-13 15:05:23 -0400

    Views and controller

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -12,18 +12,17 @@ namespace PLATES.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
-
         // 
         // GET: /HelloWorld/Welcome/ 
 
-       /* public string Welcome()
-        {
-            return "This is the Welcome action method...";
-        }*/
+        /* public string Welcome()
+         {
+             return "This is the Welcome action method...";
+         }*/
 
 
         // GET: /HelloWorld/Welcome/ 
@@ -32,9 +31,18 @@ namespace PLATES.Controllers
         {
             return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }*/
-        public string Welcome(string name, int ID = 1)
+        /*public string Welcome(string name, int ID = 1)
         {
             return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }*/
+
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
